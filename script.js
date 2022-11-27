c89edc48de7353347643f2dec219bb92ebecf409
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-11-27 10:42:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://exceptionally-classy-skunk-cascade.wayscript.cloud";
-// const baseURL = "https://nshare-server.herokuapp.com";
+const baseURL = "https://nshare-server.herokuapp.com";
 // const baseURL = "http://localhost:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
