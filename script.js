ae14958b66a8585e1e5ed07aa0614cefbbafa995
Sahil Panchasara <63531633+sahilpanchasara28@users.noreply.github.com>
2022-12-09 09:54:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://nshare-server.herokuapp.com";
+const baseURL = "https://nshare-server.onrender.com";
 // const baseURL = "http://localhost:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
